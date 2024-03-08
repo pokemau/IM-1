3127104c03a7f2aa039ce8b0b2ca31292c031bef
pokemau <rentillosa90@gmail.com>
2024-03-08 10:02:17 +0800

    add db

---
+++ db/dbtanecaf2.sql
@@ -0,0 +1,85 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 08, 2024 at 03:01 AM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.1.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `dbtañecaf2`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbluseraccount`
+--
+
+CREATE TABLE `tbluseraccount` (
+  `acctid` int(11) NOT NULL,
+  `emailadd` varchar(30) NOT NULL,
+  `username` varchar(30) NOT NULL,
+  `password` int(20) NOT NULL,
+  `usertype` varchar(10) NOT NULL DEFAULT 'user'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbluserprofile`
+--
+
+CREATE TABLE `tbluserprofile` (
+  `userid` int(5) NOT NULL,
+  `birthday` date NOT NULL DEFAULT current_timestamp(),
+  `displayname` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbluseraccount`
+--
+ALTER TABLE `tbluseraccount`
+  ADD PRIMARY KEY (`acctid`);
+
+--
+-- Indexes for table `tbluserprofile`
+--
+ALTER TABLE `tbluserprofile`
+  ADD PRIMARY KEY (`userid`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbluseraccount`
+--
+ALTER TABLE `tbluseraccount`
+  MODIFY `acctid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `tbluserprofile`
+--
+ALTER TABLE `tbluserprofile`
+  MODIFY `userid` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
